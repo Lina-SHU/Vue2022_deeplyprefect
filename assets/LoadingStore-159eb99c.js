e4d34a6e13c31fa70274331ef533b6f032bd8aed
Lina-SHU <sakura0477@gmail.com>
2023-04-03 16:13:41 +0800

    deploy

---
+++ assets/LoadingStore-159eb99c.js
@@ -0,0 +1,1 @@
+import{l as i}from"./index-d49a2ec1.js";const a=i("LoadingtStore",{state:()=>({isLoading:!1,isDisabled:!1}),actions:{toggleLoading(){this.isLoading=!this.isLoading},toggleDisabled(){this.isDisabled=!this.isDisabled}}});export{a as L};
