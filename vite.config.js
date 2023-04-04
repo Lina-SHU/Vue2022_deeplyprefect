f3bee3d314414779860d09eb7b23c780da3343a6
Lina-SHU <sakura0477@gmail.com>
2023-04-04 17:19:58 +0800

    change repo name

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === "production" ? "/Vue2022_week6/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/Vue2022_deeplyprefect/" : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
